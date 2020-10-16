8d0f5aa38d99a1e3f5bcb56b25fa666b69a6b6ea
Olga <olga1789@gmail.com>
2020-10-16 17:01:46 -0400

    Updates

---
+++ precache-manifest.f3acf0ff8ef69d7ba53941d9419aa4d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "09d871a70d6ffad13ee7",
+    "url": "/my-app/static/css/main.fa2afe2a.chunk.css"
+  },
+  {
+    "revision": "09d871a70d6ffad13ee7",
+    "url": "/my-app/static/js/main.72adfbd9.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "38e1666855570d6fa9d8",
+    "url": "/my-app/static/js/2.780667cb.chunk.js"
+  },
+  {
+    "revision": "f2ef327a7f9d8880dff018507aa88a4a",
+    "url": "/my-app/index.html"
+  }
+];
